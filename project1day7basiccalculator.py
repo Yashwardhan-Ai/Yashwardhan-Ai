ad8f7314530f3eafa0f4c5f4e06145c9a05a2d13
Yashwardhan Shinde <yashwardhanshinde25@gmail.com>
2024-01-31 22:45:08 +0530

    Add files via upload

---
+++ project1day7basiccalculator.py
@@ -0,0 +1,16 @@
+n=int(input("Enter your First number"))
+m=int(input("Enter your second number"))
+ans1=n+m
+print("Addition of",n," and",m," is ",ans1)
+ans2=n-m
+print("Substraction of ",n, "and ",m, "is ",ans2)
+ans3=n*m 
+print("Multiplication of ",n ,"and ",m, "is" ,ans3)
+ans4=n/m
+print("Division of ",n,"and",m,"is ",ans4)
+ans5=n**m
+print("Exponent of ",n ,"and ",m,"is",ans5)
+ans6=n//m
+print("Floor division of ",n,"and",m,"is",ans6)
+ans7=n%m
+print("Reminder of the division between ",n,"and",m,"is",ans7)
